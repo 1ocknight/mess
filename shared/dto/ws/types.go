e849dac5ae9561fbce67f413cc67b7f5087ab7fb
tatapomangol <deadpolmail.ru@gmail.com>
2026-01-23 00:17:11 +0500

    refactoring dto

--- shared/dto/ws/types.go
+++ shared/dto/ws/types.go
@@ -5,6 +5,6 @@ type Operation string
 const (
 	UnknownOperation Operation = "unknown"
 	SendMessage      Operation = "send_message"
-	UpdateOperation  Operation = "update_message"
+	UpdateMessage    Operation = "update_message"
 	UpdateLastRead   Operation = "update_last_read"
 )
