0feb7a38dc6b61afe191390ac5d13a5ecee327b9
tatapomangol <deadpolmail.ru@gmail.com>
2026-01-27 02:43:21 +0500

    refactoring

--- shared/auth/auth.go
+++ shared/auth/auth.go
@@ -3,7 +3,7 @@ package auth
 import "github.com/1ocknight/mess/shared/model"
 
 type Service interface {
-	SubjectExists(id string) (bool, error)
+	//SubjectExists(id string) (bool, error)
 	Verify(src string) (model.Subject, error)
 }
 
