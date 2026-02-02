43ab3284692e859b219c0a94beadcac4716d61f6
tatapomangol <deadpolmail.ru@gmail.com>
2026-02-02 13:36:21 +0500

    fix migrations

--- chat/migrations/8_refactoring_lables_message_outbox.down.sql
+++ chat/migrations/8_refactoring_lables_message_outbox.down.sql
@@ -1,5 +1,5 @@
 ALTER TABLE message_outbox
-ADD COLUMN recipients_id INT,
+ADD COLUMN recipient_id TEXT,
 DROP COLUMN chat_id,
 DROP COLUMN recipients_id,
 DROP COLUMN created_at;
--- chat/migrations/9_change_message_outbox_message_to_jsonb.down.sql
+++ chat/migrations/9_change_message_outbox_message_to_jsonb.down.sql
@@ -1,4 +1,5 @@
+-- Active: 1766314077599@@127.0.0.1@5430@chat
 -- Откатываем: возвращаем message_id, удаляем message_payload
 ALTER TABLE message_outbox 
 DROP COLUMN message_payload,
-ADD COLUMN message_id INT NOT NULL;
+ADD COLUMN message_id INT NOT NULL DEFAULT 0;
