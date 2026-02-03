e714fed5598cd4e42f786a041c987a171aba4a55
tatapomangol <deadpolmail.ru@gmail.com>
2026-02-03 19:19:54 +0500

    remove oldest data and add redis channel builder

--- shared/redisclient/channels.go
+++ shared/redisclient/channels.go
@@ -1,12 +1,46 @@
 package redisclient
 
+import "fmt"
+
 // channels type - subject:{subj_id}:chat:{chat_id}:type
 const (
-	AllValues = "*"
-
-	ChannelKeyChat    = "chat"
-	ChannelKeySubject = "subject"
-
-	ChannelTypeMessage  = "message"
-	ChannelTypeLastRead = "lastread"
+	allValues         = "*"
+	channelKeyChat    = "chat"
+	channelKeySubject = "subject"
 )
+
+type Type string
+
+func (t Type) String() string {
+	return string(t)
+}
+
+const (
+	ChannelTypeMessage  Type = "message"
+	ChannelTypeLastRead Type = "lastread"
+)
+
+func GetChannel(subjectID *string, chatID *int, channelType *Type) string {
+	subjID := allValues
+	if subjectID != nil {
+		subjID = *subjectID
+	}
+
+	chatIDStr := allValues
+	if chatID != nil {
+		chatIDStr = fmt.Sprintf("%v", *chatID)
+	}
+
+	channelTypeStr := allValues
+	if channelType != nil {
+		channelTypeStr = channelType.String()
+	}
+
+	return fmt.Sprintf("%v:%v:%v:%v:%v",
+		channelKeySubject,
+		subjID,
+		channelKeyChat,
+		chatIDStr,
+		channelTypeStr,
+	)
+}
