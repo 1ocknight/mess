7482756132c398bbe09038f1a72caf5abecee0f3
tatapomangol <deadpolmail.ru@gmail.com>
2026-02-10 23:55:16 +0500

    add channel functions

--- shared/redisclient/channels.go
+++ shared/redisclient/channels.go
@@ -1,8 +1,12 @@
+// channels type - subject:{subj_id}:chat:{chat_id}:type
 package redisclient
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-// channels type - subject:{subj_id}:chat:{chat_id}:type
 const (
 	allValues         = "*"
 	channelKeyChat    = "chat"
@@ -20,7 +24,7 @@ const (
 	ChannelTypeLastRead Type = "lastread"
 )
 
-func GetChannel(subjectID *string, chatID *int, channelType *Type) string {
+func BuildListenChannel(subjectID *string, chatID *int, channelType *Type) string {
 	subjID := allValues
 	if subjectID != nil {
 		subjID = *subjectID
@@ -43,4 +47,28 @@ func GetChannel(subjectID *string, chatID *int, channelType *Type) string {
 		chatIDStr,
 		channelTypeStr,
 	)
+}
+
+func BuildWriteChannel(subjectID string, chatID int, channelType Type) string {
+	return fmt.Sprintf("%v:%v:%v:%v:%v",
+		channelKeySubject,
+		subjectID,
+		channelKeyChat,
+		chatID,
+		channelType.String(),
+	)
+}
+
+func GetChannelInfo(ch string) (string, int, Type, error) {
+	parts := strings.Split(ch, ":")
+	if len(parts) != 5 || parts[0] != "subject" || parts[2] != "chat" {
+		return "", 0, "", fmt.Errorf("invalid channel format: %s", ch)
+	}
+
+	chatID, err := strconv.Atoi(parts[3])
+	if err != nil {
+		return "", 0, "", fmt.Errorf("invalid chat ID: %s", parts[3])
+	}
+
+	return parts[1], chatID, Type(parts[4]), nil
 }
