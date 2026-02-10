57fb0e43cbaa0cabd862689ecbd00a05bfe61ce7
tatapomangol <deadpolmail.ru@gmail.com>
2026-02-11 00:13:41 +0500

    refactoring naming

--- shared/dto/ws/types.go
+++ shared/dto/ws/types.go
@@ -3,8 +3,8 @@ package wsdto
 type Operation string
 
 const (
-	UnknownOperation Operation = "unknown"
-	SendMessage      Operation = "send_message"
-	UpdateMessage    Operation = "update_message"
-	UpdateLastRead   Operation = "update_last_read"
+	UnknownOperation        Operation = "unknown"
+	SendMessageOperation    Operation = "send_message"
+	UpdateMessageOperation  Operation = "update_message"
+	UpdateLastReadOperation Operation = "update_last_read"
 )
